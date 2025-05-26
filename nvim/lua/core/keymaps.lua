17633820e7b16db280d131b606d6da5027f1f52c
Noturn1 <arthurangeloc@gmail.com>
2025-05-26 12:27:11 -0300

    removendo sub-diretorio

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,7 +2,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Disable the spacebar key's default behavior in Normal and Visual modes
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- For conciseness
@@ -13,6 +12,8 @@ vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 
 -- save file without auto-formatting
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+vim.keymap.set("n", "<leader>c", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
 
 -- quit file
 vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
@@ -32,7 +33,7 @@ vim.keymap.set('n', 'N', 'Nzzzv', opts)
 vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts) 
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts) 
+vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
@@ -64,6 +65,12 @@ vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
 -- Stay in indent mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
+
+-- Move lines vertically while on visual mode 
+vim.keymap.set('v', '<C-j>', ':m+<CR>==', opts)
+vim.keymap.set('v', '<C-k>', ':m-<CR>==', opts)
+vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv", opts)
 
 -- Keep last yanked when pasting
 vim.keymap.set('v', 'p', '"_dP', opts)
